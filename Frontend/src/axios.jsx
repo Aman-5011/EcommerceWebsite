04ee0ede635c00207f2df45d2335f3fd154124a8
aman_singh_bhadouriya <abhadouriya501@gmail.com>
2026-07-07 05:33:26 +0530

    axios

--- Frontend/src/axios.jsx
+++ Frontend/src/axios.jsx
@@ -1,7 +1,10 @@
 import axios from 'axios';
 
 const API = axios.create({
-    baseURL: import.meta.env.VITE_BASE_URL
+    baseURL: import.meta.env.VITE_BASE_URL,
 });
+
+// ADD THIS LINE
+console.log("My API URL is:", import.meta.env.VITE_BASE_URL);
 
 export default API;
